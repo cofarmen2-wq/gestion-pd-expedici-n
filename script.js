f81ef85b2c7c7657dc6bbb1a0d603ebfbd8d1501
cofarmen2-wq <cofarmen2@gmail.com>
2026-08-27 12:21:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 
+
 // URL de tu aplicación web publicada en Google Apps Script
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyB7yRevFu4p_SLgUL-cWE_jjcgMqvU_FzyK1YJCmK3Agm3D1Atg7sEUSv0qmLlHseNQ/exec";
 
