5e2fc57762fd9350b8d94a837d4dd0c99c3b2578
cofarmen2-wq <cofarmen2@gmail.com>
2026-08-28 17:32:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyB7yRevFu4p_SLgUL-cWE_jjcgMqvU_FzyK1YJCmK3Agm3D1Atg7sEUSv0qmLKlHseNQ/exec";
+const documentos = [];
 const documentos = [];
 let lectorQr = null;
 let lectorQrIngreso = null;
